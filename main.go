01b84ba6a5387e21f67087ed4698a772a852ddb2
Aliaksandr Kantsevoi <aliaksandrkantsevoi@gmail.com>
2023-10-29 13:55:28 +0100

    split main file

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sort"
-	"strconv"
 	"strings"
 	"time"
 
@@ -22,7 +20,7 @@ func main() {
 		}
 
 		if !info.IsDir() && strings.HasSuffix(path, ".md") {
-			props, err := parseMarkdownFile(path)
+			props, err := extractFrontmatterProperties(path)
 			if err != nil {
 				fmt.Printf("Error parsing file %s: %v\n", path, err)
 			}
@@ -39,10 +37,6 @@ func main() {
 		fmt.Println("Error walking the directory:", err)
 	}
 
-	sort.Slice(todayResults, func(i, j int) bool {
-		return todayResults[i].ReportKey > todayResults[j].ReportKey
-	})
-
 	writeCSVUpdateIfNeeded(time.Now().Format("2006-01-02"), todayResults)
 }
 
@@ -55,7 +49,7 @@ type Properties struct {
 	Done           map[string]int
 }
 
-func parseMarkdownFile(filePath string) (*Properties, error) {
+func extractFrontmatterProperties(filePath string) (*Properties, error) {
 	data, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, err
@@ -82,50 +76,4 @@ func parseMarkdownFile(filePath string) (*Properties, error) {
 	shouldExist(filePath, fm, "reportKey")
 
 	return mapToProperties(fm), nil
-}
-
-func mapToProperties(input map[string]string) *Properties {
-	suffix := "_sub_done_today"
-
-	pr := Properties{
-		ReportKey:      input["reportKey"],
-		Priority:       toInt(input["priority"]),
-		Status:         input["status"],
-		TomatoesPerDay: toInt(input["tomatosPerDay"]),
-		Done:           map[string]int{},
-	}
-
-	if v, ok := input["doneToday"]; ok {
-		pr.DoneTotal = toInt(v)
-	} else {
-		for k, v := range input {
-			name, hasSuffix := strings.CutSuffix(k, suffix)
-			if !hasSuffix {
-				continue
-			}
-
-			// log.Printf("%v %T %v\n", k, v, v)
-
-			increment := toInt(v)
-			pr.DoneTotal += increment
-			pr.Done[name] = increment
-		}
-	}
-
-	return &pr
-}
-
-func toInt(v string) int {
-	i, err := strconv.Atoi(v)
-	if err != nil {
-		panic(fmt.Errorf("parseInt: %v: %w", v, err))
-	}
-	return i
-}
-
-func shouldExist(path string, m map[string]string, key string) {
-	if _, ok := m[key]; !ok {
-		fmt.Printf("no %v in %v\n%v", key, path, m)
-		os.Exit(1)
-	}
 }
---
+++ props_parse.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	// detect that it's done but for subproject
+	suffix = "_sub_done_today"
+)
+
+func mapToProperties(input map[string]string) *Properties {
+
+	pr := Properties{
+		ReportKey:      input["reportKey"],
+		Priority:       toInt(input["priority"]),
+		Status:         input["status"],
+		TomatoesPerDay: toInt(input["tomatosPerDay"]),
+		Done:           map[string]int{},
+	}
+
+	if v, ok := input["doneToday"]; ok {
+		pr.DoneTotal = toInt(v)
+	} else {
+		for k, v := range input {
+			name, hasSuffix := strings.CutSuffix(k, suffix)
+			if !hasSuffix {
+				continue
+			}
+
+			// log.Printf("%v %T %v\n", k, v, v)
+
+			increment := toInt(v)
+			pr.DoneTotal += increment
+			pr.Done[name] = increment
+		}
+	}
+
+	return &pr
+}
+
+func toInt(v string) int {
+	i, err := strconv.Atoi(v)
+	if err != nil {
+		panic(fmt.Errorf("parseInt: %v: %w", v, err))
+	}
+	return i
+}
+
+func shouldExist(path string, m map[string]string, key string) {
+	if _, ok := m[key]; !ok {
+		fmt.Printf("no %v in %v\n%v", key, path, m)
+		os.Exit(1)
+	}
+}
