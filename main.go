9e6172ba261f7530a0628f9b0caf309f551b6faf
Aliaksandr Kantsevoi <aliaksandrkantsevoi@gmail.com>
2023-11-12 13:26:21 +0100

    add report/reset separation

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"time"
 )
@@ -14,13 +15,37 @@ var (
 	hoursPerTomato = float64(0.0)
 )
 
+type Command string
+
+const (
+	commandReport Command = "report"
+	commandReset  Command = "reset"
+)
+
+var allCommands = []Command{
+	commandReport,
+	commandReset,
+}
+
 func main() {
 
+	commandStr := flag.String("command", "", fmt.Sprintf("what to do {%v}", allCommands))
 	sourcePath := flag.String("source", "", "path to the root folder of projects")
 	hoursPT := flag.Float64("hpert", 1.0, "hours per reported tomato")
 	writeFolder := flag.String("result", "", "where to save the result\nIf empty - it will be stored in the same folder as source")
 
 	flag.Parse()
+
+	if len(*commandStr) == 0 {
+		fmt.Println("command is mandatoty")
+		os.Exit(1)
+	}
+
+	command := Command(*commandStr)
+	if !slices.Contains(allCommands, command) {
+		fmt.Println("unsupported command: ", command)
+		os.Exit(1)
+	}
 
 	if len(*writeFolder) == 0 {
 		writeFolder = sourcePath
@@ -33,14 +58,20 @@ func main() {
 	folderPath = *sourcePath
 	hoursPerTomato = *hoursPT
 
-	// work
 	paths, err := markdownFilePaths(folderPath)
 	stopIfErrf("%w", err)
 
-	todayResults, err := extractPropsFromFiles(paths)
-	stopIfErrf("%w", err)
+	switch command {
+	case commandReport:
+		todayResults, err := extractPropsFromFiles(paths)
+		stopIfErrf("report: %w", err)
 
-	writeCSVUpdateIfNeeded(time.Now().Format("2006-01-02"), *writeFolder, todayResults)
+		writeCSVUpdateIfNeeded(time.Now().Format("2006-01-02"), *writeFolder, todayResults)
+	case commandReset:
+		err := resetProgress(paths)
+		stopIfErrf("reset: %w", err)
+	}
+
 }
 
 func extractPropsFromFiles(filePaths []string) ([]Properties, error) {
